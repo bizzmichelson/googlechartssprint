79b858ac498083b1c9ba4c46aa6d9c1bdcbb9218
Bizz Michelson <bizz.michelson@gmail.com>
2018-01-09 14:35:15 -0600

    deleted unused files

--- public/controllers/basics.controller.js
+++ public/controllers/basics.controller.js
@@ -4,56 +4,40 @@
    vm.click = function () {
      console.log('BasicsController', vm.input);
 
-     var newIntake = {
-       description: vm.input.born,
-       description: vm.input.basicHighDesc,
-       date: vm.input.basicHighYear,
-       description: vm.input.basicLowDescription,
-       date: vm.input.basicLowYear,
-     }
-     basicHighDesc
-     :
-     "sdg"
-     basicHighYear
-     :
-     2012
-     basicLowDesc
-     :
-     "dsgsgg"
-     basicLowYear
-     :
-     2011
-     born
-     :
-     1991
-
-     if (
-       newIntake.input.born == '' ||
-       newIntake.input.basicHighDescription == '' ||
-       newIntake.basicHighYear == '' ||
-       newIntake.input.basicLowDescription == '' ||
-       newIntake.input.basicLowYear == ''
-     ) {
-       swal({
-         title: 'Please complete all required fields.',
-         icon: "warning",
-         width: 600,
-         padding: 100,
-         background: '#fff url(assets/page.JPG)'
-       })
-     } else {
-       console.log('newIntake', newIntake);
-       swal({
-         title: 'Required fields submitted to database.',
-         icon: "success",
-         width: 600,
-         padding: 100,
-         background: '#fff url(assets/page.JPG)'
-       }).then(function () {
+    //  var newIntake = {
+    //    description: vm.born,
+    //    description: vm.basicHighDesc,
+    //    date: vm.basicHighYear,
+    //    description: vm.basicLowDescription,
+    //    date: vm.basicLowYear,
+    //  }
+    //  if (
+    //    newIntake.born == null ||
+    //    newIntake.basicHighDesc == null  ||
+    //    newIntake.basicHighYear == null  ||
+    //    newIntake.basicLowDesc == null  ||
+    //    newIntake.basicLowYear == null )
+    //  {
+    //      swal({
+    //      title: 'Please complete all required fields.',
+    //      icon: "warning",
+    //      width: 600,
+    //      padding: 100,
+    //      background: '#fff url(assets/page.JPG)'
+    //    })
+    //  } else {
+    //   //  console.log('newIntake', newIntake);
+    //    swal({
+    //      title: 'Required fields submitted to database.',
+    //      icon: "success",
+    //      width: 600,
+    //      padding: 100,
+    //      background: '#fff url(assets/page.JPG)'
+    //    }).then(function () {
          dataService.postBasics(vm.input);
-       });
+  //      });
      }
-   };
+  //  };
 
 
  });
